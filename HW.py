186259ec915b34ba206b90d5dec4827865ed5c01
AleksejIstomin <82605561+AleksejIstomin@users.noreply.github.com>
2022-04-25 17:52:08 +0300

    Add files via upload

--- HW.py
+++ HW.py
@@ -4,22 +4,28 @@
 # Tiek parādīts kas ir uzvarējis – dators vai lietotājs, vai neizšķirts.
 
 user_input = int(input('Please enter: Rock - 0, Paper - 1 or Scissors - 2: '))
-rock = Rock = 0
-paper = Paper = 1
-scissors = Scissors = 2
-
+rock = 0
+paper = 1
+scissors = 2
+if 0:
+    print(f'Rock')
 import random
 computer_choice = random.randint(0, 2)
 print(computer_choice)
-criteria_0 = 0 == 0
-criteria_1 = 1 == 1
-criteria_2 = 2 == 2
-criteria_3 = 0 > 2
-criteria_4 = 0 < 1
-criteria_5 = 1 < 2
-criteria_6 = 1 > 0
-criteria_7 = 2 > 1
-criteria_8 = 2 < 1
+criteria_0 = rock == rock
+criteria_1 = paper == paper
+criteria_2 = scissors == scissors
+criteria_3 = rock > scissors
+criteria_4 = rock < paper
+criteria_5 = paper < scissors
+criteria_6 = paper > rock
+criteria_7 = scissors > paper
+criteria_8 = scissors < rock
 if criteria_0 or criteria_1 or criteria_2:
     print(user_input, 'vs', computer_choice)
-    print('No one wins')
+    # print('No one wins')
+elif criteria_3 or criteria_6 or criteria_7:
+    print(user_input, 'vs', computer_choice)
+    # print('User wins, computer loose')
+elif criteria_4 or criteria_5 or criteria_8:
+    print(user_input, 'vs', computer_choice)
